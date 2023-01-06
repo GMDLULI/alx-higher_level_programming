b56268036f29049df589504a387a6671133d985c
GMDLULI <gugulethumdluli98@gmail.com>
2023-01-06 00:25:01 +0000

    corrections

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 # Author - Gugulethu mdluli
 
 def print_last_digit(number):
-	print(abs(number) % 10, end="")
-	return (abs(number) % 10)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
