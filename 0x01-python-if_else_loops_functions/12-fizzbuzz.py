dcb3649510204ac8d03e4cba02f4f976c8b0ed7d
GMDLULI <gugulethumdluli98@gmail.com>
2023-01-06 01:23:18 +0000

    corrections

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 # Author- Gugulethu Mdluli
-"""print the numbers from 1 to 1000 separated by a space
-   for multiples of three, print fizz instead of the number
-   for multiples of five, print BUzz instead of the number.
-   for multiples of both three and five print FizzBuzz instead of the number
 
 
-   """
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
