3e9cfb0bad4db3e2185ba2e27c4322a171f8a005
GMDLULI <gugulethumdluli98@gmail.com>
2023-01-07 03:32:16 +0000

    calculations

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My additon funtion
+    Args:
+        a:  first interger
+        b: second interger
+    Returns:
+        the return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My additon funtion
+    Args: 
+        a: first interger
+        b: second interger
+    Returns:
+        the return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My additon funtion
+    Args: 
+        a: first interger
+        b: second interger
+    Returns:
+        the return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My additon funtion
+    Args: 
+        a: first interger
+        b: second interger
+    Returns:
+        the return value. a / b
+    """
+    return (a / b)
