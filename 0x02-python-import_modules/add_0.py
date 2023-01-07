e80d2c87c4c5ee7619d09aae7e0250bacc90b70d
GMDLULI <gugulethumdluli98@gmail.com>
2023-01-07 02:44:34 +0000

    first commit

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-
 def add(a, b):
+    """My additon function
 
+    Args:
+        a: first integr
+       	b: second integer
+
+    Returns:
+	    the return value. a + b
+	"""
     return (a + b)
