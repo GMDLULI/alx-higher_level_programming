2914fdbc38837be81f9364b42adec69127362d68
GMDLULI <gugulethumdluli98@gmail.com>
2023-01-31 04:31:54 +0000

    print rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -13,8 +13,7 @@ class Rectangle:
         TypeError: if height or width is not an integer
         ValueError: if height or width is < 0
     Return:
-        area of rectangle
-        parimeter of rectangle 
+        print rectangle with character "#"
     """
     def __init__(self, width=0, height=0):
         self.__width = width
@@ -55,9 +54,25 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns the area of the rectangle"""
         return self.__height * self.__width
 
     def perimeter(self):
+        """Returns th perimeter of the rectangle"""
         return ((self.__height *2) + (self.__width *2))
-        if height == 0 or width == 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
+
+    def __str__(self)-> str:
+        """Presents a diagram of the rectangle defined fron object"""
+        if self.__height == 0 or self.__width ==0:
+            return ("")
+
+        rectangle = ""
+        for i in range(self.__width):
+            for d in range(self.__height):
+                rectangle += "#"
+
+            if i < self.__height - 1:
+                rectangle += '\n'
+        return (rectangle)
