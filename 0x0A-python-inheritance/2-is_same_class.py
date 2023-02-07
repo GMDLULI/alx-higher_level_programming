35c66ad3f3be493a272c93c3d41e0da96bc96464
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-07 03:11:13 +0000

    check instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module checks if instance is of class"""
+
+def is_same_class(obj, a_class):
+    """Retur true if object is instance of class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
