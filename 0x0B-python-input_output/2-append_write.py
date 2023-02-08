d967ed9fc2d0ca0431c88f8ab150b9407f507597
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-08 19:39:22 +0000

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a text file-appending function"""
+
+
+def append_write(filename="", text=""):
+    """FUnction that appends a string at the end of text file"""
+    with open('filename', 'a', encoding="utf-8") as f
+    print(f.write(), end="")
