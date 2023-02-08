8246bcd55634c87e8bdb0060a81e260965ff4721
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-08 19:23:44 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This modules defines a text file-reading function"""
+
+
+def read_file(filename=""):
+    """Prints the contents of the text file"""
+    with open("filename", "utf-8") as f
+    print(f.read(), end="")
