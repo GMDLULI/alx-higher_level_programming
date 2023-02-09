2630be775ccf19e234c05114a108aa483c6b56c7
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-09 02:51:00 +0000

    create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module defines a JSON file-reading function"""
+
+
+def load_from_json_file(filename):
+    """Creates a python object from a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
