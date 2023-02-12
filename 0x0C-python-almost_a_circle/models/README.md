6a29469c4dee88c7d65af3651b3251ed4368b98f
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-12 01:36:15 +0000

    models

---
+++ 0x0C-python-almost_a_circle/models/README.md
@@ -0,0 +1,13 @@
+# Models 
+
+This folder contains three classes:
+------------------------------------------
+* ** Base: the base for all Models:
+  * Defines the id of objects and keeps a record of all models created.
+
+* ** Rectangle:
+  * Inhherits from Base.
+
+* ** Square:
+  * Inherits from rectangle. 
+
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines a class Base"""
+
+
+class Base:
+    """class Base that manages id attribute in all future classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """inistializes new Base.
+        Args:
+            id(int): the identity of the new base
+        """
+
+        if id is None:
+        self.id = id
+        else:
+            Base.__nb_object +=1
+            self.id = Base.__nb_objects
+           
