d00f0076a0a5c47b6669f50ab034be315f8dfa9b
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-12 02:25:29 +0000

    rectangle attributes

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/python3
+"""
+Module that defines a class Rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(base):
+    """class REctangle implements Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initilize Rectangle class
+        Args:
+            width: width
+            height: height
+            x: x variable
+            y: y varible 
+        """
+        super().__init__(id) 
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @getter
+    def width(self):
+        """getter function for with
+           returns: width
+        """
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """setter function for width
+         Args:
+            value (int): value to be set
+         """
+        self.__width = value
+    
+    @getter
+    def height(self):
+        """getter function for height
+           returns: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter function for height
+         Args:
+            value (int): value to be set
+         """
+        self.__height = value
+
+
+    @getter
+    def x(self):
+        """getter function for x
+           returns: x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter function for x
+         Args:
+            value (int): value to be set
+         """
+        self.__width = value
+
+
+    @getter
+    def y(self):
+        """getter function for y
+           returns: y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter function for x
+         Args:
+            value (int): value to be set
+         """
+        self.__y = value
