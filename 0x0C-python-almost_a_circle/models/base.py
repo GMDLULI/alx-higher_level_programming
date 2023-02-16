f8c83ac3acccbc18baab96b3272fcf151608d319
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-16 04:52:49 +0000

    corrections

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,7 +23,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """function that returns the JSON string representation
         """
-        if list_dictionaries is None or list_dictionaries = "[]":
+        if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         else:
             return json.dumps(list_dictionaries)
